7076591339deebb4413ace31443d289adea8d093
Anoop G <anoop261996@gmail.com>
2025-01-27 11:42:02 +0530

    splash screen ui completed, intro page ui completed

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String basepath = 'assets/images/';
+  
+
+  static const String introBG = '${basepath}intro_bg.png';
+
+  static const String chooseModeBG = '${basepath}choose_mode_bg.png';
+
+}
